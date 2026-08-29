393941c048a711d56fbeada13ea89c1447cc30e8
Narciso Gomes <narciso.4265@aluno.pr.senac.br>
2026-08-28 21:13:25 -0300

    feat: adiciona arquivo de operadores aritmeticos

---
+++ autod/arithmetic_operators.py
@@ -0,0 +1,50 @@
+#n1 = 10
+#n2 = 3
+
+print('Informe dois números: ')
+n1 = float(input('Primeiro número (n1): '))
+n2 = float(input('Segundo número (n2): '))
+
+print('')
+
+print("# Adição (+)")
+r = n1 + n2
+print(n1,"+",n2,"=",r)
+
+print('')
+
+print("# Subtração (-)")
+r = n1 - n2
+print(n1,"-",n2,"=",r)
+
+print('')
+
+
+print("# Divisão (/)")
+r = n1 / n2
+print(n1,"/",n2,"=",r)
+
+print('')
+
+print("# Divisão Inteira (//)")
+r = n1 // n2
+print(n1,"//",n2,"=",r)
+
+print('')
+
+print('# Resto de Divisão (%)')
+r = n1 % n2
+print(n1,"%",n2,"=",r)
+
+print('')
+
+print('# Multiplicação (*)')
+r = n1 * n2
+print(n1,"*",n2,"=",r)
+
+print('')
+
+print('# Potenciação (**)')
+r = n1 ** n2
+print(n1,"**",n2,"=",r)
+
